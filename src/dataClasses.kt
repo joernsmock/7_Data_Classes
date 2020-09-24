51e6d47c65014b47750ceb23497fe5743139ce8c
Jörn Smock <jsmock@posteo.de>
2020-09-24 22:13:19 +0200

    cannot extract (and print) parts of a string?

--- src/dataClasses.kt
+++ src/dataClasses.kt
@@ -1,16 +1,23 @@
 fun main() {
     data class Recipe(val title: String, val isVegetarian: Boolean) {
     }
+
     var r1 = Recipe("chicken bhuna", false)
     var r2 = Recipe("falafel", true)
     var r3 = Recipe("puttanesa sauce", false)
     var r4 = Recipe("chicken bhuna", false)
 
-    val recipes = arrayOf( r1, r2, r3, r4)
+    val recipes = arrayOf(r1, r2, r3, r4)
     for (i in recipes) {
-        println(i.hashCode())
-        if (r1 == r4) {
-            println("r1 equals r4")
+        for (j in recipes) {
+            if (i !== j) {
+                val iLen = i.hashCode().toString().length
+                val h1 = i.hashCode().toString().takeLast(6)
+
+                val jLen = j.hashCode().toString().length
+                val h2 = j.hashCode().toString().takeLast(6)
+                println("$h1 !== $h2")
+            }
         }
     }
 }
