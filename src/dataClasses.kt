92bf2676357ce13dd037c86c811d4f15adf8a2e1
Jörn Smock <jsmock@posteo.de>
2020-09-25 21:59:43 +0200

    still dunno howto introspect

--- src/dataClasses.kt
+++ src/dataClasses.kt
@@ -8,16 +8,20 @@ fun main() {
     var r4 = Recipe("chicken bhuna", false)
 
     val recipes = arrayOf(r1, r2, r3, r4)
+    var n = 0
+    var m = 0
     for (i in recipes) {
         for (j in recipes) {
-            if (i !== j) {
-                val iLen = i.hashCode().toString().length
-                val h1 = i.hashCode().toString().takeLast(6)
-
-                val jLen = j.hashCode().toString().length
-                val h2 = j.hashCode().toString().takeLast(6)
-                println("$h1 !== $h2")
+            if (!(i === j) and (i == j)) {
+                // because 2 recipes have same content, 2 are printed?
+                // can I find-out/print the name of the r-variable too?
+                val hash1 = i.hashCode().toString().takeLast(3)
+                val hash2 = j.hashCode().toString().takeLast(3)
+                println("n=$n/m=$m => ${i.title} === ${j.title};  hashes: ..$hash1, ..$hash2")
             }
+            m = m + 1
         }
+        n = n + 1
+        m = 0
     }
 }
